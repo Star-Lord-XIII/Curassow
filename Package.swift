22e2904c2051ba65bb612be50b5fdd45e69d3f1a
Chintan Ghate <chintanghate@gmail.com>
2023-07-21 03:34:34 +0100

    Update swift-tools-version to 5.3 as 3.1.0 is no longer supported

--- Package.swift
+++ Package.swift
@@ -1,16 +1,27 @@
-import PackageDescription
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import PackageDescription
 
 let package = Package(
   name: "Curassow",
-  targets: [
-    Target(name: "example", dependencies: ["Curassow"]),
+  products: [
+    .library(name: "Curassow", targets: ["Curassow"])
   ],
   dependencies: [
-    .Package(url: "https://github.com/nestproject/Nest.git", majorVersion: 0, minor: 4),
-    .Package(url: "https://github.com/nestproject/Inquiline.git", majorVersion: 0, minor: 4),
-    .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 6),
-    .Package(url: "https://github.com/kylef/fd.git", majorVersion: 0, minor: 2),
-    .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 7),
+    .package(name: "Nest", url: "https://github.com/nestproject/Nest.git", from: "0.4.0"),
+    .package(name: "Inquiline", url: "https://github.com/nestproject/Inquiline.git", from: "0.4.0"),
+    .package(name: "Commander", url: "https://github.com/kylef/Commander.git", from: "0.6.0"),
+    .package(name: "fd", url: "https://github.com/kylef/fd.git", from: "0.2.0"),
+    .package(name: "Spectre", url: "https://github.com/kylef/Spectre.git", from: "0.7.0"),
+  ],
+  targets: [
+    .target(name: "Curassow", dependencies: [
+        "Nest",
+        "Inquiline",
+        "Commander",
+        "fd",
+        "Spectre",
+    ]),
   ]
 )
